2c5273f7651770f2e7383edfdcfb98695597d8e8
MatinAk <matin.akbari.899@gmail.com>
2017-09-16 13:48:14 -0700

    Angular Material design && theme.scss added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MdAutocompleteModule , MdCheckboxModule , MdDatepickerModule , MdInputModule ,
+  MdRadioModule, MdSelectModule, MdSliderModule , MdSlideToggleModule,
+  MdMenuModule, MdSidenavModule, MdToolbarModule, MdListModule, MdGridListModule, MdCardModule ,
+  MdTabsModule, MdExpansionModule, MdButtonModule, MdButtonToggleModule, MdChipsModule,
+  MdIconModule, MdProgressSpinnerModule, MdProgressBarModule, MdDialogModule, MdTooltipModule,
+  MdSnackBarModule, MdTableModule, MdSortModule , MdPaginatorModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import 'hammerjs';
+
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdAutocompleteModule,
+    MdCheckboxModule,
+    MdDatepickerModule,
+    MdInputModule,
+    MdRadioModule,
+    MdSelectModule,
+    MdSliderModule,
+    MdSlideToggleModule,
+    MdMenuModule,
+    MdSidenavModule,
+    MdToolbarModule,
+    MdListModule,
+    MdGridListModule,
+    MdCardModule ,
+    MdTabsModule,
+    MdExpansionModule,
+    MdButtonModule,
+    MdButtonToggleModule,
+    MdChipsModule,
+    MdIconModule,
+    MdProgressSpinnerModule,
+    MdProgressBarModule,
+    MdDialogModule,
+    MdTooltipModule,
+    MdSnackBarModule,
+    MdTableModule,
+    MdSortModule,
+    MdPaginatorModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
